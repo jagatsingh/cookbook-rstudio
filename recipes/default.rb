1ed6c529ae063ced13f1582e72360b1283fc2ce7
Mario Rodas <rodasmario2@gmail.com>
2012-09-17 23:00:27 -0500

    Refactoring

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,30 @@
 
 include_recipe 'R'
 
-case node['platform']
+rstudio_package = "#{Chef::Config[:file_cache_path]}/#{node.rstudio[:package]}"
+
+remote_file rstudio_package do
+  source node.rstudio[:download_url]
+  mode 0644
+  checksum node.rstudio[:checksum]
+end
+
+case node[:platform]
 when 'ubuntu', 'debian'
   %w| libssl0.9.8 libapparmor1 apparmor-utils |.each do |pkg|
     package pkg do
       action :install
     end
+  end
+
+  dpkg_package 'rstudio-server' do
+    source rstudio_package
+    action :install
+  end
+when 'centos', 'redhat', 'amazon', 'scientific'
+  yum_package 'rstudio-server' do
+    source rstudio_package
+    action :install
   end
 end
 
@@ -23,25 +41,4 @@ directory "/home/rstudio" do
   group "users"
   mode 0755
   action :create
-end
-
-rstudio_package = "#{Chef::Config[:file_cache_path]}/#{node.rstudio[:package]}"
-
-remote_file rstudio_package do
-  source node.rstudio[:download_url]
-  mode 0644
-  checksum node.rstudio[:checksum]
-end
-
-case node['platform']
-when 'ubuntu', 'debian'
-  dpkg_package 'rstudio-server' do
-    source rstudio_package
-    action :install
-  end
-when 'centos', 'redhat'
-  yum_package 'rstudio-server' do
-    source rstudio_package
-    action :install
-  end
 end
